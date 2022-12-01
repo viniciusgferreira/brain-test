3d8b311e4fcbfcb9a46be1f559bda2fcfbd49207
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-01 11:26:13 -0300

    merge conflict

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< Updated upstream
   baseURL: 'https://4d80-177-55-195-175.sa.ngrok.io/',
-=======
-  baseURL: 'http://localhost:3000/',
->>>>>>> Stashed changes
 });
 
 export default api;
